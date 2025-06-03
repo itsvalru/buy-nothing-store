4aed3b9e0b3d3666a0b70115f72af1ae383252cc
itsvalru <79265002+itsvalru@users.noreply.github.com>
2025-06-03 16:06:10 +0200

    .

--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "@/components/SignUpForm";
-
-export default function SignupPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-6">
-      <SignUpForm />
-    </main>
-  );
-}
